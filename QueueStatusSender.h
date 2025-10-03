f4f3ed9780b8122fce2f8a80469d7047bb7342fb
Simone Di Biase <s.dibiase4@studenti.unimol.it>
2025-10-03 14:14:51 +0200

    modifica app QuequeStatusSender

--- QueueStatusSender.h
+++ QueueStatusSender.h
@@ -1,3 +1,5 @@
+#include "action.h"
+
 #include "ns3/application.h"
 #include "ns3/ipv6-address.h"
 #include "ns3/ptr.h"
@@ -11,14 +13,26 @@ class QueueStatusApp : public Application
     QueueStatusApp();
     virtual ~QueueStatusApp();
 
-    void Setup(Ptr<NetDevice> device, Ipv6Address source, Ipv6Address destination);
+    void Setup(Ipv6Address source,
+               Ipv6Address destination,
+               std::string nameSource,
+               std::string nameDestination,
+               std::shared_ptr<std::vector<std::vector<Action>>> q_registerSource,
+               int32_t indexNodeDestination);
 
   private:
     virtual void StartApplication() override;
     virtual void StopApplication() override;
 
+
     void SendQueueStatus();
     void ScheduleNextQueueStatus();
+    std::vector<uint32_t> selectQRegisterLines(int32_t indexNodeDestination,
+                                               std::string nameSource);
+    uint32_t GetMinQValueInRow(const std::vector<Action>& row);
+    void PrintQRegisterForNode(
+        const std::string& nameSource,
+        const std::shared_ptr<std::vector<std::vector<Action>>>& q_registerSource);
 
     EventId m_sendEvent;
     Ptr<NetDevice> m_device;
@@ -26,4 +40,8 @@ class QueueStatusApp : public Application
     Ipv6Address m_sourceAddress;
     bool m_running;
     Ptr<Socket> m_socket;
+    std:: string m_nameSource;
+    std:: string m_nameDestination;
+    std::shared_ptr<std::vector<std::vector<Action>>> m_q_registerSource;
+    std:: int32_t m_indexNodeDestination;
 };
